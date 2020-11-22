f8f5e754bf5da569457edc06570fbaf8f7f355e3
marwin1991 <peter.zmilczak@gmail.com>
2020-11-22 11:17:09 +0100

    Created simple flask project

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/stories')
 def index():
+    user_stories = user_story.get_user_stories()
+    headers = user_story.get_headers()
+    return render_template("index.html", stories=user_stories, headers=headers)
+
+
+@app.route('/stories/<id>')
+def get_user_story():
     user_stories = user_story.get_user_stories()
     headers = user_story.get_headers()
     return render_template("index.html", stories=user_stories, headers=headers)
